89ecad0e86b80678a1617419bbd47206e6293505
Lee A. Butler <iraytrace@gmail.com>
2015-09-12 00:29:44 -0400

    Lots of context menus in 3d view

--- src/Osg3dView.cpp
+++ src/Osg3dView.cpp
@@ -6,13 +6,18 @@
 #include <osg/LightModel>
 #include <osgViewer/Renderer>
 
+static bool debugView = false;
+#define vDebug if (debugView) qDebug
+
 Osg3dView::Osg3dView(QWidget *parent)
     : QOpenGLWidget(parent)
     , m_viewingCore(new ViewingCore)
+    , m_mouseMode(MM_ORBIT)
 {
     setContextMenuPolicy(Qt::CustomContextMenu);
     connect(this, SIGNAL(customContextMenuRequested(QPoint)),
             this, SLOT(customMenuRequested(QPoint)));
+    buildPopupMenu();
 
     // Construct the embedded graphics window
     m_osgGraphicsWindow = new osgViewer::GraphicsWindowEmbedded(0,0,width(),height());
@@ -40,7 +45,6 @@ Osg3dView::Osg3dView(QWidget *parent)
 
 void Osg3dView::setScene(OsgItemModel *model)
 {
-    qDebug("setScene");
     connect(model, SIGNAL(rowsInserted(QModelIndex,int,int)),
             this, SLOT(fitScreenTopView(QModelIndex,int,int)));
     connect(model, SIGNAL(dataChanged(QModelIndex,QModelIndex,QVector<int>)),
@@ -53,7 +57,7 @@ void Osg3dView::setScene(OsgItemModel *model)
 
 void Osg3dView::paintGL()
 {
-    qDebug("paintGL");
+    vDebug("paintGL");
 
     // Update the camera
     osg::Camera *cam = this->getCamera();
@@ -66,11 +70,13 @@ void Osg3dView::paintGL()
 
     // Invoke the OSG traversal pipeline
     frame();
+
+    emit updated();
 }
 
 void Osg3dView::resizeGL(int w, int h)
 {
-    qDebug("resizeGL");
+    vDebug("resizeGL");
 
     m_osgGraphicsWindow->getEventQueue()->windowResize(9, 0, w, h);
     m_osgGraphicsWindow->resized(0,0,w,h);
@@ -81,41 +87,164 @@ void Osg3dView::hello()
     qDebug("hello");
 }
 
-void Osg3dView::mousePressEvent(QMouseEvent *event)
+void Osg3dView::setMouseMode(Osg3dView::MouseMode mode)
 {
-    qDebug("mousePressEvent");
-    if (event->button() == Qt::RightButton)
-        customMenuRequested(event->pos());
+    m_mouseMode = mode;
+
+    if(mode == MM_ROTATE)
+        m_viewingCore->setViewingCoreMode( ViewingCore::FIRST_PERSON );
+    else {
+        m_viewingCore->setViewingCoreMode( ViewingCore::THIRD_PERSON );
+    }
+
+    emit mouseModeChanged(mode);
 }
 
-void Osg3dView::mouseReleaseEvent(QMouseEvent *event)
+osg::Vec2d Osg3dView::getNormalized(const int ix, const int iy)
 {
-    qDebug("mouseReleaseEvent");
+    int x, y, width, height;
+    osg::Vec2d ndc;
+
+    // we don't really need the x, y values but the width/height are important
+    m_osgGraphicsWindow->getWindowRectangle(x, y, width, height);
+    int center = width/2;
+    ndc[0] = ((double)ix - (double)center) / (double)center;
+    if (ndc[0] > 1.0) ndc[0] = 1.0;
+
+    center = height/2;
+    int invertedY = height - iy;
+    ndc[1] = ((double)invertedY - (double)center) / (double)center;
+    if (ndc[1] > 1.0) ndc[1] = 1.0;
+
+    return ndc;
+}
+
+void Osg3dView::mousePressEvent(QMouseEvent *event)
+{
+    vDebug("mousePressEvent");
+
+    if (event->button() == Qt::LeftButton) {
+        m_savedEventNDCoords = getNormalized(event->x(), event->y());
+
+        // Do the job asked
+        if (m_mouseMode & (MM_PAN|MM_ROTATE|MM_ORBIT|MM_ZOOM) )
+            m_viewingCore->setPanStart( m_savedEventNDCoords.x(),
+                                        m_savedEventNDCoords.y());
+        else if (m_mouseMode & MM_PICK_CENTER) {
+            m_viewingCore->pickCenter(m_savedEventNDCoords.x(),
+                                      m_savedEventNDCoords.y() );
+        }
+    }
 }
 
 void Osg3dView::mouseMoveEvent(QMouseEvent *event)
 {
-    qDebug("mouseMoveEvent");
+    vDebug("mouseMoveEvent");
+    osg::Vec2d currentNDC = getNormalized(event->x(), event->y());
+    osg::Vec2d delta = currentNDC - m_savedEventNDCoords;
+
+    switch (m_mouseMode) {
+    case MM_ORBIT:
+        m_viewingCore->rotate( m_savedEventNDCoords, delta);
+        break;
+    case MM_PAN:
+        m_viewingCore->pan(delta.x(), delta.y());
+        break;
+    case MM_ZOOM: {
+        double tempScale = m_viewingCore->getFovyScale();
+        m_viewingCore->setFovyScale(1.03);
+
+        if(delta.y() > 0)
+            m_viewingCore->fovyScaleDown();
+
+        if(delta.y() < 0)
+            m_viewingCore->fovyScaleUp();
+
+        m_viewingCore->setFovyScale(tempScale);
+        break;
+    }
+    case MM_ROTATE:
+        m_viewingCore->rotate(  m_savedEventNDCoords, delta );
+        break;
+    default:
+        break;
+    }
+
+    m_savedEventNDCoords = currentNDC;
+    update();
 }
+
+void Osg3dView::mouseReleaseEvent(QMouseEvent *event)
+{
+    vDebug("mouseReleaseEvent");
+    m_savedEventNDCoords = getNormalized(event->x(), event->y());
+}
+
 
 void Osg3dView::wheelEvent(QWheelEvent *event)
 {
+    if(event->delta() > 0)
+        m_viewingCore->dolly(0.5);
+    else
+        m_viewingCore->dolly(-0.5);
+    update();
+}
 
+void Osg3dView::buildPopupMenu()
+{
+    QAction *a;
+    QMenu *sub = m_popupMenu.addMenu("MouseMode...");
+
+    a = sub->addAction("Orbit", this, SLOT(setMouseMode()));
+    a->setData(QVariant(MM_ORBIT));
+    a = sub->addAction("Pan", this, SLOT(setMouseMode()));
+    a->setData(QVariant(MM_PAN));
+    a = sub->addAction("Rotate", this, SLOT(setMouseMode()));
+    a->setData(QVariant(MM_ROTATE));
+    a = sub->addAction("Zoom", this, SLOT(setMouseMode()));
+    a->setData(QVariant(MM_ZOOM));
+    a = sub->addAction("Pick Center", this, SLOT(setMouseMode()));
+    a->setData(QVariant(MM_PICK_CENTER));
+
+    sub = m_popupMenu.addMenu("Std View...");
+    a = sub->addAction("Top", this, SLOT(setStandardView()));
+    a->setData(V_TOP);
+    a = sub->addAction("Bottom", this, SLOT(setStandardView()));
+    a->setData(V_BOTTOM);
+    a = sub->addAction("Front", this, SLOT(setStandardView()));
+    a->setData(V_FRONT);
+    a = sub->addAction("Back", this, SLOT(setStandardView()));
+    a->setData(V_BACK);
+    a = sub->addAction("Right", this, SLOT(setStandardView()));
+    a->setData(V_RIGHT);
+    a = sub->addAction("Left", this, SLOT(setStandardView()));
+    a->setData(V_LEFT);
+
+    sub = m_popupMenu.addMenu("Projection...");
+    a = sub->addAction("Orthographic", this, SLOT(setProjection()));
+    a->setData(P_ORTHO);
+    a = sub->addAction("Perspective", this, SLOT(setProjection()));
+    a->setData(P_PERSP);
+
+    sub = m_popupMenu.addMenu("DrawMode...");
+    a = sub->addAction("Facets", this, SLOT(setDrawMode()));
+    a->setData(D_FACET);
+    a = sub->addAction("Wireframe", this, SLOT(setDrawMode()));
+    a->setData(D_WIRE);
+    a = sub->addAction("Points", this, SLOT(setDrawMode()));
+    a->setData(D_POINT);
 }
 
 void Osg3dView::customMenuRequested(const QPoint &pos)
 {
-    qDebug("customMenu");
-    QMenu *menu=new QMenu(this);
-    menu->addAction(new QAction("Action 1", this));
-    menu->addAction(new QAction("Action 2", this));
-    menu->addAction(new QAction("Action 3", this));
-    menu->popup(this->mapToGlobal(pos));
+    vDebug("customMenu %d %d", pos.x(), pos.y());
+
+    m_popupMenu.popup(this->mapToGlobal(pos));
 }
 
 void Osg3dView::fitScreenTopView(const QModelIndex &parent, int first, int last)
 {
-    qDebug("rowsInserted");
+    vDebug("rowsInserted");
     m_viewingCore->viewTop();
     m_viewingCore->fitToScreen();
     update();
@@ -123,7 +252,7 @@ void Osg3dView::fitScreenTopView(const QModelIndex &parent, int first, int last)
 
 void Osg3dView::dataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight, const QVector<int> &roles)
 {
-    qDebug("dataChanged");
+    vDebug("dataChanged");
     update();
 }
 
@@ -143,3 +272,66 @@ void Osg3dView::setLightingTwoSided()
     }
 }
 
+
+void Osg3dView::setMouseMode()
+{
+    QAction *a = dynamic_cast<QAction *>(sender());
+    if (!a)
+        return;
+
+    MouseMode mode = static_cast<MouseMode>(a->data().toUInt());
+    setMouseMode(mode);
+}
+
+void Osg3dView::setStandardView()
+{
+    QAction *a = dynamic_cast<QAction *>(sender());
+    if (!a)
+        return;
+
+    StandardView view = static_cast<StandardView>(a->data().toUInt());
+    switch (view) {
+    case V_TOP: m_viewingCore->viewTop(); break;
+    case V_BOTTOM: m_viewingCore->viewBottom(); break;
+    case V_FRONT: m_viewingCore->viewFront(); break;
+    case V_BACK: m_viewingCore->viewBack(); break;
+    case V_RIGHT: m_viewingCore->viewRight(); break;
+    case V_LEFT: m_viewingCore->viewLeft(); break;
+    }
+    update();
+}
+
+void Osg3dView::setDrawMode()
+{
+    QAction *a = dynamic_cast<QAction *>(sender());
+    if (!a)
+        return;
+
+    DrawMode drawMode = static_cast<DrawMode>(a->data().toUInt());
+    switch (drawMode) {
+    case D_FACET:
+        break;
+    case D_WIRE:
+        break;
+    case D_POINT:
+        break;
+    }
+}
+
+void Osg3dView::setProjection()
+{
+    QAction *a = dynamic_cast<QAction *>(sender());
+    if (!a)
+        return;
+
+    Projection projType = static_cast<Projection>(a->data().toUInt());
+    switch (projType) {
+    case P_ORTHO:
+        m_viewingCore->setOrtho(true);
+        break;
+    case P_PERSP:
+        m_viewingCore->setOrtho(false);
+        break;
+    }
+    update();
+}
--- src/Osg3dView.h
+++ src/Osg3dView.h
@@ -3,9 +3,12 @@
 
 #include <QOpenGLWidget>
 #include <QMouseEvent>
+#include <QMenu>
+
 #include <osgViewer/Viewer>
 
 #include "ViewingCore.h"
+
 class OsgItemModel;
 
 class Osg3dView : public QOpenGLWidget, public osgViewer::Viewer
@@ -15,13 +18,47 @@ class Osg3dView : public QOpenGLWidget, public osgViewer::Viewer
 public:
     Osg3dView(QWidget * parent = 0);
 
+    enum MouseMode {
+        MM_ORBIT = (1<<1),
+        MM_PAN = (1<<2),
+        MM_ZOOM = (1<<3),
+        MM_ROTATE = (1<<4),
+        MM_PICK_CENTER = (1<<5)
+    };
+    enum StandardView {
+        V_TOP = (1<<0),
+        V_BOTTOM = (1<<1),
+        V_FRONT = (1<<2),
+        V_BACK = (1<<3),
+        V_RIGHT = (1<<4),
+        V_LEFT = (1<<5)
+    };
+    enum Projection {
+        P_ORTHO = (1<<0),
+        P_PERSP = (1<<1)
+    };
+    enum DrawMode {
+        D_FACET = (1<<1),
+        D_WIRE = (1<<2),
+        D_POINT = (1<<3)
+    };
+
     /// Let others tell what scene graph we should be drawing
     void setScene(OsgItemModel *model);
+
+    osg::ref_ptr<ViewingCore> getViewingCore() const { return m_viewingCore; }
 
 public slots:
     void paintGL();
     void resizeGL(int w, int h);
     void hello();
+    /// Set the m_mouseMode variable
+    void setMouseMode(MouseMode mode);
+    void setMouseMode();
+    void setStandardView();
+    void setDrawMode();
+    void setProjection();
+
     void mousePressEvent( QMouseEvent* event );
     void mouseReleaseEvent(QMouseEvent* event);
     void mouseMoveEvent(QMouseEvent* event);
@@ -33,7 +70,15 @@ public slots:
     void dataChanged(const QModelIndex & topLeft,
                      const QModelIndex & bottomRight,
                      const QVector<int> & roles = QVector<int> ());
+
+signals:
+    /// Let the rest of the world (OsgView) know the current MouseMode
+    void mouseModeChanged(Osg3dView::MouseMode);
+    void updated();
+
 private:
+    osg::Vec2d getNormalized(const int ix, const int iy);
+
     /// OSG uses singleSided drawing/display by default.
     /// This is annoying when you are "inside" something and the back wall of
     /// it simply disappears. This gets called to set up to draw both front and
@@ -41,11 +86,20 @@ private:
     /// Yes it's a performance hit.
     void setLightingTwoSided();
 
+    void buildPopupMenu();
+
+    QMenu m_popupMenu;
+
     /// OSG graphics window
     osg::ref_ptr<osgViewer::GraphicsWindowEmbedded> m_osgGraphicsWindow;
 
     /// Camera manager
     osg::ref_ptr<ViewingCore> m_viewingCore;
+
+    /// Current mouse mode
+    MouseMode m_mouseMode;
+
+    osg::Vec2d m_savedEventNDCoords;
 };
 
 #endif // OSGVIEW_H
